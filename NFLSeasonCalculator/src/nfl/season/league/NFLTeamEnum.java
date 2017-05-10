8113759add491a02fce785c8d088e70dec1654ef
Craig Romanowski <romojr50@gmail.com>
2017-05-10 07:14:09 -0500

    Add default Elo Ratings to NFL Team Enum

--- NFLSeasonCalculator/src/nfl/season/league/NFLTeamEnum.java
+++ NFLSeasonCalculator/src/nfl/season/league/NFLTeamEnum.java
@@ -2,59 +2,61 @@ package nfl.season.league;
 
 public enum NFLTeamEnum {
 
-	PATRIOTS("Patriots", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 1, 18),
-	DOLPHINS("Dolphins", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 13, 4),
-	JETS("Jets", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 32, 13),
-	BILLS("Bills", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 26, 15),
+	PATRIOTS("Patriots", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 1, 1779, 18),
+	DOLPHINS("Dolphins", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 13, 1510, 4),
+	JETS("Jets", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 32, 1425, 13),
+	BILLS("Bills", NFLConferenceEnum.AFC, NFLDivisionEnum.EAST, 26, 1474, 15),
 	
-	STEELERS("Steelers", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 3, 19),
-	RAVENS("Ravens", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 21, 33),
-	BENGALS("Bengals", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 25, 18),
-	BROWNS("Browns", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 29, 18),
+	STEELERS("Steelers", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 3, 1646, 19),
+	RAVENS("Ravens", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 21, 1484, 33),
+	BENGALS("Bengals", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 25, 1521, 18),
+	BROWNS("Browns", NFLConferenceEnum.AFC, NFLDivisionEnum.NORTH, 29, 1251, 18),
 	
-	TEXANS("Texans", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 14, 20),
-	COLTS("Colts", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 18, 10),
-	TITANS("Titans", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 11, 6),
-	JAGUARS("Jaguars", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 27, 16),
+	TEXANS("Texans", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 14, 1501, 20),
+	COLTS("Colts", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 18, 1518, 10),
+	TITANS("Titans", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 11, 1437, 6),
+	JAGUARS("Jaguars", NFLConferenceEnum.AFC, NFLDivisionEnum.SOUTH, 27, 1320, 16),
 	
-	BRONCOS("Broncos", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 9, 13),
-	RAIDERS("Raiders", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 5, 5),
-	CHIEFS("Chiefs", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 12, 5),
-	CHARGERS("Chargers", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 24, 16),
+	BRONCOS("Broncos", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 9, 1581, 13),
+	RAIDERS("Raiders", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 5, 1542, 5),
+	CHIEFS("Chiefs", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 12, 1667, 5),
+	CHARGERS("Chargers", NFLConferenceEnum.AFC, NFLDivisionEnum.WEST, 24, 1403, 16),
 	
-	GIANTS("Giants", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 8, 5),
-	COWBOYS("Cowboys", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 6, 0),
-	EAGLES("Eagles", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 16, 2),
-	REDSKINS("Redskins", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 19, 7),
+	GIANTS("Giants", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 8, 1543, 5),
+	COWBOYS("Cowboys", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 6, 1602, 0),
+	EAGLES("Eagles", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 16, 1514, 2),
+	REDSKINS("Redskins", NFLConferenceEnum.NFC, NFLDivisionEnum.EAST, 19, 1504, 7),
 	
-	PACKERS("Packers", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 4, 23),
-	VIKINGS("Vikings", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 23, 29),
-	BEARS("Bears", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 31, 10),
-	LIONS("Lions", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 17, 19),
+	PACKERS("Packers", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 4, 1628, 23),
+	VIKINGS("Vikings", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 23, 1495, 29),
+	BEARS("Bears", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 31, 1323, 10),
+	LIONS("Lions", NFLConferenceEnum.NFC, NFLDivisionEnum.NORTH, 17, 1499, 19),
 	
-	FALCONS("Falcons", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 2, 16),
-	SAINTS("Saints", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 22, 14),
-	PANTHERS("Panthers", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 15, 12),
-	BUCCANEERS("Buccaneers", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 10, 4),
+	FALCONS("Falcons", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 2, 1673, 16),
+	SAINTS("Saints", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 22, 1495, 14),
+	PANTHERS("Panthers", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 15, 1538, 12),
+	BUCCANEERS("Buccaneers", NFLConferenceEnum.NFC, NFLDivisionEnum.SOUTH, 10, 1506, 4),
 	
-	SEAHAWKS("Seahawks", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 7, 28),
-	CARDINALS("Cardinals", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 20, 26),
-	NINERS("49ers", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 30, 19),
-	RAMS("Rams", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 28, 7);
+	SEAHAWKS("Seahawks", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 7, 1604, 28),
+	CARDINALS("Cardinals", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 20, 1553, 26),
+	NINERS("49ers", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 30, 1278, 19),
+	RAMS("Rams", NFLConferenceEnum.NFC, NFLDivisionEnum.WEST, 28, 1346, 7);
 	
 	private String teamName;
 	private NFLConferenceEnum conference;
 	private NFLDivisionEnum division;
 	private int defaultPowerRanking;
+	private int defaultEloRating;
 	private int defaultHomeFieldAdvantage;
 	
 	private NFLTeamEnum(String teamName, NFLConferenceEnum conference, 
-			NFLDivisionEnum division, int defaultPowerRanking, 
+			NFLDivisionEnum division, int defaultPowerRanking, int defaultEloRating, 
 			int defaultHomeFieldAdvantage) {
 		this.teamName = teamName;
 		this.conference = conference;
 		this.division = division;
 		this.defaultPowerRanking = defaultPowerRanking;
+		this.defaultEloRating = defaultEloRating;
 		this.defaultHomeFieldAdvantage = defaultHomeFieldAdvantage;
 	}
 
@@ -88,6 +90,14 @@ public enum NFLTeamEnum {
 	
 	public void setDefaultPowerRanking(int defaultPowerRanking) {
 		this.defaultPowerRanking = defaultPowerRanking;
+	}
+	
+	public int getDefaultEloRating() {
+		return defaultEloRating;
+	}
+	
+	public void setDefaultEloRating(int defaultEloRating) {
+		this.defaultEloRating = defaultEloRating;
 	}
 	
 	public int getDefaultHomeFieldAdvantage() {
