8ae46973c95ef4dc377d6f62281e3e6196b3ac39
Craig Romanowski <romojr50@gmail.com>
2017-06-05 07:44:23 -0500

    Add tiebreakers up to common games

--- NFLSeasonCalculator/src/nfl/season/season/NFLTiebreaker.java
+++ NFLSeasonCalculator/src/nfl/season/season/NFLTiebreaker.java
@@ -1,26 +1,208 @@
 package nfl.season.season;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nfl.season.league.Team;
+
 public class NFLTiebreaker {
 
 	public static NFLSeasonTeam tiebreakTeams(NFLSeasonTeam team1,
 			NFLSeasonTeam team2) {
 		NFLSeasonTeam tieWinner = null;
 		
-		double team1WinPercent = calculateTeamWinPercent(team1);
-		double team2WinPercent = calculateTeamWinPercent(team2);
+		double team1WinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team1.getNumberOfWins(), team1.getNumberOfLosses(), team1.getNumberOfTies());
+		double team2WinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team2.getNumberOfWins(), team2.getNumberOfLosses(), team2.getNumberOfTies());
 		
 		if (team1WinPercent > team2WinPercent) {
 			tieWinner = team1;
 		} else if (team2WinPercent > team1WinPercent) {
 			tieWinner = team2;
+		} else {
+			Team leagueTeam1 = team1.getTeam();
+			Team leagueTeam2 = team2.getTeam();
+			String team1Name = leagueTeam1.getName();
+			String team2Name = leagueTeam2.getName();
+			
+			tieWinner = resolveHeadToHeadTiebreak(team1, team2);
+			
+			if (tieWinner == null) {
+				tieWinner = resolveDivisionWinPercentTieBreak(team1, team2);
+				
+				if (tieWinner == null) {
+					tieWinner = resolveCommonGameWinPercentTieBreak(team1, team2);
+				}
+			}
 		}
 		
 		return tieWinner;
 	}
 	
-	private static double calculateTeamWinPercent(NFLSeasonTeam team) {
-		return (team.getNumberOfWins() + (team.getNumberOfTies() / 2.0)) / 
-				(team.getNumberOfWins() + team.getNumberOfLosses() + team.getNumberOfTies());
+	private static double calculateWinPercentFromWinsLossesAndTies(int wins, 
+			int losses, int ties) {
+		return (wins + (ties / 2.0)) / (wins + losses + ties);
+	}
+	
+
+	private static NFLSeasonTeam resolveHeadToHeadTiebreak(NFLSeasonTeam team1,
+			NFLSeasonTeam team2) {
+		NFLSeasonTeam tieWinner = null;
+		
+		Team leagueTeam1 = team1.getTeam();
+		Team leagueTeam2 = team2.getTeam();
+		String team1Name = leagueTeam1.getName();
+		String team2Name = leagueTeam2.getName();
+		
+		List<String> team1Wins = team1.getWinsAgainst();
+		List<String> team1Losses = team1.getLossesAgainst();
+		List<String> team1Ties = team1.getTiesAgainst();
+		double team1HeadToHeadWinPercent = getHeadToHeadWinPercent(team2Name,
+				team1Wins, team1Losses, team1Ties);
+		
+		List<String> team2Wins = team2.getWinsAgainst();
+		List<String> team2Losses = team2.getLossesAgainst();
+		List<String> team2Ties = team2.getTiesAgainst();
+		double team2HeadToHeadWinPercent = getHeadToHeadWinPercent(team1Name,
+				team2Wins, team2Losses, team2Ties);
+		
+		if (team1HeadToHeadWinPercent > team2HeadToHeadWinPercent) {
+			tieWinner = team1;
+		} else if (team2HeadToHeadWinPercent > team1HeadToHeadWinPercent) {
+			tieWinner = team2;
+		}
+		
+		return tieWinner;
+	}
+
+	private static NFLSeasonTeam resolveDivisionWinPercentTieBreak(
+			NFLSeasonTeam team1, NFLSeasonTeam team2) {
+		NFLSeasonTeam tieWinner = null;
+		
+		double team1DivisionWinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team1.getNumberOfDivisionWins(), team1.getNumberOfDivisionLosses(), 
+				team1.getNumberOfDivisionTies());
+		double team2DivisionWinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team2.getNumberOfDivisionWins(), team2.getNumberOfDivisionLosses(), 
+				team2.getNumberOfDivisionTies());
+		
+		if (team1DivisionWinPercent > team2DivisionWinPercent) {
+			tieWinner = team1;
+		} else if (team2DivisionWinPercent > team1DivisionWinPercent) {
+			tieWinner = team2;
+		}
+		return tieWinner;
+	}
+	
+	private static NFLSeasonTeam resolveCommonGameWinPercentTieBreak(
+			NFLSeasonTeam team1, NFLSeasonTeam team2) {
+		NFLSeasonTeam tieWinner = null;
+		
+		List<String> team1WinsAgainst = team1.getWinsAgainst();
+		List<String> team1LossesAgainst = team1.getLossesAgainst();
+		List<String> team1TiesAgainst = team1.getTiesAgainst();
+		
+		List<String> team2WinsAgainst = team2.getWinsAgainst();
+		List<String> team2LossesAgainst = team2.getLossesAgainst();
+		List<String> team2TiesAgainst = team2.getTiesAgainst();
+		
+		List<String> commonGames = getListOfCommonGames(team1, team2);
+		int team1CommonWins = 0;
+		int team1CommonLosses = 0;
+		int team1CommonTies = 0;
+		int team2CommonWins = 0;
+		int team2CommonLosses = 0;
+		int team2CommonTies = 0;
+		
+		for (String commonGame : commonGames) {
+			team1CommonWins += numberOfTimesInList(team1WinsAgainst, commonGame);
+			team1CommonLosses += numberOfTimesInList(team1LossesAgainst, commonGame);
+			team1CommonTies += numberOfTimesInList(team1TiesAgainst, commonGame);
+			team2CommonWins += numberOfTimesInList(team2WinsAgainst, commonGame);
+			team2CommonLosses += numberOfTimesInList(team2LossesAgainst, commonGame);
+			team2CommonTies += numberOfTimesInList(team2TiesAgainst, commonGame);
+		}
+		
+		double team1CommonWinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team1CommonWins, team1CommonLosses, team1CommonTies);
+		double team2CommonWinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team2CommonWins, team2CommonLosses, team2CommonTies);
+		
+		if (team1CommonWinPercent > team2CommonWinPercent) {
+			tieWinner = team1;
+		} else if (team2CommonWinPercent > team1CommonWinPercent) {
+			tieWinner = team2;
+		}
+		
+		return tieWinner;
+	}
+
+	private static double getHeadToHeadWinPercent(String team2Name,
+			List<String> team1Wins, List<String> team1Losses,
+			List<String> team1Ties) {
+		int team1HeadToHeadWins = numberOfTimesInList(team1Wins, team2Name);
+		int team1HeadToHeadLosses = numberOfTimesInList(team1Losses, team2Name);
+		int team1HeadToHeadTies = numberOfTimesInList(team1Ties, team2Name);
+		double team1HeadToHeadWinPercent = calculateWinPercentFromWinsLossesAndTies(
+				team1HeadToHeadWins, team1HeadToHeadLosses, team1HeadToHeadTies);
+		return team1HeadToHeadWinPercent;
+	}
+
+	private static List<String> getListOfCommonGames(NFLSeasonTeam team1,
+			NFLSeasonTeam team2) {
+		List<String> commonGames = new ArrayList<String>();
+		
+		List<String> team1WinsAgainst = team1.getWinsAgainst();
+		List<String> team1LossesAgainst = team1.getLossesAgainst();
+		List<String> team1TiesAgainst = team1.getTiesAgainst();
+		
+		for (String team1WinAgainst : team1WinsAgainst) {
+			if (!commonGames.contains(team1WinAgainst) && teamHasPlayedAgainstOpponent(team2, team1WinAgainst)) {
+				commonGames.add(team1WinAgainst);
+			}
+		}
+		for (String team1LossAgainst : team1LossesAgainst) {
+			if (!commonGames.contains(team1LossAgainst) && teamHasPlayedAgainstOpponent(team2, team1LossAgainst)) {
+				commonGames.add(team1LossAgainst);
+			}
+		}
+		for (String team1TieAgainst : team1TiesAgainst) {
+			if (!commonGames.contains(team1TieAgainst) && teamHasPlayedAgainstOpponent(team2, team1TieAgainst)) {
+				commonGames.add(team1TieAgainst);
+			}
+		}
+		
+		return commonGames;
+	}
+	
+	private static boolean teamHasPlayedAgainstOpponent(NFLSeasonTeam team, 
+			String opponentName) {
+		boolean teamHasPlayedOpponent = false;
+		
+		List<String> teamWins = team.getWinsAgainst();
+		List<String> teamLosses = team.getLossesAgainst();
+		List<String> teamTies = team.getTiesAgainst();
+		
+		if (teamWins.contains(opponentName) || teamLosses.contains(opponentName)
+				|| teamTies.contains(opponentName)) {
+			teamHasPlayedOpponent = true;
+		}
+		
+		return teamHasPlayedOpponent;
+	}
+	
+	private static int numberOfTimesInList(List<String> list,
+			String element) {
+		int numberOfTimesInList = 0;
+		
+		for (String listItem : list) {
+			if (listItem != null && listItem.equalsIgnoreCase(element)) {
+				numberOfTimesInList++;
+			}
+		}
+		
+		return numberOfTimesInList;
 	}
 
 }
--- NFLSeasonCalculator/test/nfl/season/season/NFLTiebreakerTest.java
+++ NFLSeasonCalculator/test/nfl/season/season/NFLTiebreakerTest.java
@@ -3,6 +3,11 @@ package nfl.season.season;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nfl.season.league.Team;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,10 +21,70 @@ public class NFLTiebreakerTest {
 	private NFLSeasonTeam team1_1;
 	
 	@Mock
+	private Team leagueTeam1_1;
+	
+	private String team1_1Name = "Team 1 - 1";
+	
+	@Mock
 	private NFLSeasonTeam team1_2;
+	
+	@Mock
+	private Team leagueTeam1_2;
+	
+	private String team1_2Name = "Team 1 - 2";
+	
+	private String team1_3Name = "Team 1 - 3";
+	
+	private String team2_1Name = "Team 2 - 1";
+	
+	private String team2_2Name = "Team 2 - 2";
+	
+	private String team2_3Name = "Team 2 - 3";
+	
+	private String team3_1Name = "Team 3 - 1";
 	
 	@Before
 	public void setUp() {
+		when(team1_1.getTeam()).thenReturn(leagueTeam1_1);
+		when(leagueTeam1_1.getName()).thenReturn("Team 1 - 1");
+		when(team1_2.getTeam()).thenReturn(leagueTeam1_2);
+		when(leagueTeam1_2.getName()).thenReturn("Team 1 - 2");
+		
+		when(team1_1.getNumberOfWins()).thenReturn(9);
+		when(team1_1.getNumberOfLosses()).thenReturn(7);
+		when(team1_1.getNumberOfDivisionWins()).thenReturn(5);
+		when(team1_1.getNumberOfDivisionLosses()).thenReturn(3);
+		
+		when(team1_2.getNumberOfWins()).thenReturn(9);
+		when(team1_2.getNumberOfLosses()).thenReturn(7);
+		when(team1_2.getNumberOfDivisionWins()).thenReturn(5);
+		when(team1_2.getNumberOfDivisionLosses()).thenReturn(3);
+		
+		setUpTeamWinsLossesTiesAgainst();
+	}
+
+	private void setUpTeamWinsLossesTiesAgainst() {
+		List<String> team1_1Wins = new ArrayList<String>();
+		List<String> team1_1Losses = new ArrayList<String>();
+		List<String> team1_1Ties = new ArrayList<String>();
+		
+		team1_1Wins.add(leagueTeam1_2.getName());
+		team1_1Losses.add(leagueTeam1_2.getName());
+		
+		when(team1_1.getWinsAgainst()).thenReturn(team1_1Wins);
+		when(team1_1.getLossesAgainst()).thenReturn(team1_1Losses);
+		when(team1_1.getTiesAgainst()).thenReturn(team1_1Ties);
+		
+		List<String> team1_2Wins = new ArrayList<String>();
+		List<String> team1_2Losses = new ArrayList<String>();
+		List<String> team1_2Ties = new ArrayList<String>();
+		
+		team1_2Wins.add(leagueTeam1_1.getName());
+		team1_2Losses.add(leagueTeam1_1.getName());
+		
+		when(team1_2.getWinsAgainst()).thenReturn(team1_2Wins);
+		when(team1_2.getLossesAgainst()).thenReturn(team1_2Losses);
+		when(team1_2.getTiesAgainst()).thenReturn(team1_2Ties);
 	}
 	
 	@Test
@@ -45,6 +110,90 @@ public class NFLTiebreakerTest {
 		when(team1_2.getNumberOfLosses()).thenReturn(8);
 		
 		assertEquals(team1_1, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
+	}
+	
+	@Test
+	public void divisionTieBreakBetweenTwoTeamsBrokenByHeadToHead() {
+		List<String> team1_1TempWins = new ArrayList<String>();
+		team1_1TempWins.add(leagueTeam1_2.getName());
+		team1_1TempWins.add(leagueTeam1_2.getName());
+		when(team1_1.getWinsAgainst()).thenReturn(team1_1TempWins);
+		
+		List<String> team1_2TempLosses = new ArrayList<String>();
+		team1_2TempLosses.add(leagueTeam1_1.getName());
+		team1_2TempLosses.add(leagueTeam1_1.getName());
+		when(team1_2.getLossesAgainst()).thenReturn(team1_2TempLosses);
+		
+		assertEquals(team1_1, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
+	}
+	
+	@Test
+	public void divisionTieBreakBetweenTwoTeamsBrokenByDivisionWinPercent() {
+		when(team1_1.getNumberOfDivisionWins()).thenReturn(5);
+		when(team1_1.getNumberOfDivisionLosses()).thenReturn(3);
+		when(team1_1.getNumberOfDivisionTies()).thenReturn(0);
+		
+		when(team1_2.getNumberOfDivisionWins()).thenReturn(4);
+		when(team1_2.getNumberOfDivisionLosses()).thenReturn(4);
+		when(team1_2.getNumberOfDivisionTies()).thenReturn(0);
+		
+		assertEquals(team1_1, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
+		
+		when(team1_1.getNumberOfDivisionWins()).thenReturn(4);
+		when(team1_1.getNumberOfDivisionLosses()).thenReturn(4);
+		when(team1_1.getNumberOfDivisionTies()).thenReturn(0);
+		
+		when(team1_2.getNumberOfDivisionWins()).thenReturn(4);
+		when(team1_2.getNumberOfDivisionLosses()).thenReturn(3);
+		when(team1_2.getNumberOfDivisionTies()).thenReturn(0);
+		
+		assertEquals(team1_2, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
+		
+		when(team1_1.getNumberOfDivisionWins()).thenReturn(5);
+		when(team1_1.getNumberOfDivisionLosses()).thenReturn(2);
+		when(team1_1.getNumberOfDivisionTies()).thenReturn(1);
+		
+		when(team1_2.getNumberOfDivisionWins()).thenReturn(5);
+		when(team1_2.getNumberOfDivisionLosses()).thenReturn(3);
+		when(team1_2.getNumberOfDivisionTies()).thenReturn(0);
+		
+		assertEquals(team1_1, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
+	}
+	
+	@Test
+	public void divisionTieBreakBetweenTwoTiesBrokenByCommonGamesRecord() {
+		List<String> team1_1TempWins = new ArrayList<String>();
+		List<String> team1_1TempLosses = new ArrayList<String>();
+		List<String> team1_1TempTies = new ArrayList<String>();
+		
+		team1_1TempWins.add(team1_3Name);
+		team1_1TempWins.add(team2_1Name);
+		team1_1TempLosses.add(team2_2Name);
+		team1_1TempLosses.add(team2_3Name);
+		
+		List<String> team1_2TempWins = new ArrayList<String>();
+		List<String> team1_2TempLosses = new ArrayList<String>();
+		List<String> team1_2TempTies = new ArrayList<String>();
+		
+		team1_2TempWins.add(team2_2Name);
+		team1_2TempWins.add(team3_1Name);
+		team1_2TempLosses.add(team1_3Name);
+		team1_2TempLosses.add(team2_1Name);
+		
+		when(team1_1.getWinsAgainst()).thenReturn(team1_1TempWins);
+		when(team1_1.getLossesAgainst()).thenReturn(team1_1TempLosses);
+		when(team1_1.getTiesAgainst()).thenReturn(team1_1TempTies);
+		
+		when(team1_2.getWinsAgainst()).thenReturn(team1_2TempWins);
+		when(team1_2.getLossesAgainst()).thenReturn(team1_2TempLosses);
+		when(team1_2.getTiesAgainst()).thenReturn(team1_2TempTies);
+		
+		assertEquals(team1_1, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
+		
+		team1_1TempLosses.add(team3_1Name);
+		team1_2TempTies.add(team2_3Name);
+		
+		assertEquals(team1_2, NFLTiebreaker.tiebreakTeams(team1_1, team1_2));
 	}
 	
 }
