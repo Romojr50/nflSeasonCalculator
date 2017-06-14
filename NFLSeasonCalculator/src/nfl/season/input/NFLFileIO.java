295bb96a208d871b048e20922e49bb01c39d9ab5
Craig Romanowski <romojr50@gmail.com>
2017-06-14 06:32:40 -0500

    Create folders for settings if they don't exist

--- NFLSeasonCalculator/src/nfl/season/input/NFLFileIO.java
+++ NFLSeasonCalculator/src/nfl/season/input/NFLFileIO.java
@@ -2,13 +2,22 @@ package nfl.season.input;
 
 public abstract class NFLFileIO {
 
+	protected static final String NFL_TEAM_SETTINGS_FOLDER_LOCATION = 
+			"TeamSettings";
+	
 	protected static final String NFL_TEAM_SETTINGS_FILE_LOCATION = 
-			"TeamSettings/nflTeamSettings.txt";
+			NFL_TEAM_SETTINGS_FOLDER_LOCATION + "/nflTeamSettings.txt";
+	
+	protected static final String NFL_PLAYOFF_SETTINGS_FOLDER_LOCATION = 
+			"PlayoffSettings";
 	
 	protected static final String NFL_PLAYOFF_SETTINGS_FILE_LOCATION = 
-			"PlayoffSettings/nflPlayoffSettings.txt";
+			NFL_PLAYOFF_SETTINGS_FOLDER_LOCATION + "/nflPlayoffSettings.txt";
+	
+	protected static final String NFL_SEASON_SAVE_FOLDER_LOCATION = 
+			"SeasonSave";
 	
 	protected static final String NFL_SEASON_SAVE_FILE_LOCATION = 
-			"SeasonSave/nflRegularSeason.txt";
+			NFL_SEASON_SAVE_FOLDER_LOCATION + "/nflRegularSeason.txt";
 	
 }
--- NFLSeasonCalculator/src/nfl/season/input/NFLFileWriterFactory.java
+++ NFLSeasonCalculator/src/nfl/season/input/NFLFileWriterFactory.java
@@ -1,20 +1,31 @@
 package nfl.season.input;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class NFLFileWriterFactory extends NFLFileIO {
 
 	public FileOutputStream createNFLTeamSettingsWriter() throws FileNotFoundException {
+		createFolderIfItDoesNotExist(NFL_TEAM_SETTINGS_FOLDER_LOCATION);
 		return createNFLFileWriter(NFL_TEAM_SETTINGS_FILE_LOCATION);
 	}
 
 	public FileOutputStream createNFLPlayoffSettingsWriter() throws FileNotFoundException {
+		createFolderIfItDoesNotExist(NFL_PLAYOFF_SETTINGS_FOLDER_LOCATION);
 		return createNFLFileWriter(NFL_PLAYOFF_SETTINGS_FILE_LOCATION);
 	}
 	
 	public FileOutputStream createNFLSeasonSaveWriter() throws FileNotFoundException {
+		createFolderIfItDoesNotExist(NFL_SEASON_SAVE_FOLDER_LOCATION);
 		return createNFLFileWriter(NFL_SEASON_SAVE_FILE_LOCATION);
+	}
+	
+	private void createFolderIfItDoesNotExist(String folderLocation) {
+		File directory = new File(folderLocation);
+	    if (!directory.exists()){
+	        directory.mkdir();
+	    }
 	}
 	
 	private FileOutputStream createNFLFileWriter(String fileLocation) 
